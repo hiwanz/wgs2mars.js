b6755becb7ee8527b5ccd88abfc17e61a998acef
hiwanz <princeb4d@gmail.com>
2020-04-22 20:17:57 +0800

    publish package to github package

--- test/test.js
+++ test/test.js
@@ -6,6 +6,7 @@ describe('Transform WGS-84 to GCJ-02', function () {
 
   it('Get right value', function (done) {
     var gcjloc = transform(119.3122312,26.0240049);
+
     assert.equal(119.31705425869873, gcjloc.lng);
     assert.equal(26.02096344048847, gcjloc.lat);
     done();
